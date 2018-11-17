14f244995adaabe6db4f4646a15eb7c3d378ab93
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-17 04:08:42 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  i = 0
+  dwarves.collect do |name|
+    puts "#{i + 1} #{name}"
+  i += 1
+  end
+end
+def summon_captain_planet(planeteer_calls)
+    planeteer_calls.collect do |names|
+      names.capitalize + "!"
+     end
+  end
+
+
+def long_planeteer_calls(calls_long)
+  if calls_long.length > 4
+    true
+  else
+    false
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese(cheddar_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheddar_cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end

fd95396a6d330f4cc7e63c70e3a29b63b6a5dc81
Diana Badas <diana.badas@gmail.com>
2018-12-26 15:07:47 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (array)
+  results = ""
+  array.each_with_index do |name, index|
+    results << "#{index + 1}. #{name}"
+  end
+
+  puts results
 end
 
 def summon_captain_planet# code an argument here

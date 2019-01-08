cc8b26ba82c7a97ef3c2987f31e514bc00b0e3a6
Aaron Berns <anberns@gmail.com>
2019-01-08 19:20:23 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  
+  array.each_with_index do |value, index|
+    puts "#{index + 1}. #{value}"
+  end
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
+  array.collect do |name|
+    name = name.capitalize + "!"
+  end
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
+  i = 0 
+  values = []
+  array.each do |value|
+    values << value.length > 4
+  end
+  
+  values.any? do |value|
+    value.true 
+  end
+  
 end
 
 def find_the_cheese# code an argument here

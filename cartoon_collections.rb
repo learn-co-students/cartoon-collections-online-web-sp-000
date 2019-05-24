cbf31616dcf05c70329fc53523604a22d5ead71e
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2019-05-24 01:01:52 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(names)
+  dwarf_names = []
+  names.each_with_index do |dwarves,index|
+    dwarf_names<< "#{index+1}. #{dwarves}"
+    puts dwarf_names.join(" ")
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  elements = []
+  array.map do |thing|
+    elements<<thing.capitalize + "!"
+    puts elements
+  end
 end
+
 
 def long_planeteer_calls# code an argument here
   # Your code here

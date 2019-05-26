4ff51e9241ca0e33a02f2066e2ce9fe6eca7b4d9
kevin lang <langkevina@gmail.com>
2019-05-26 15:56:44 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,43 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  cnt = 0
+  array.each do |item|
+    cnt += 1
+    puts("#{cnt}.  " + item)
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  # .capitalize
+  i = 0
+ block_return_values = []
+ while i < array.length
+  #  block_return_values << array[i]
+   block_return_values[i] = array[i].capitalize + "!"
+   i = i + 1
+ end
+  return block_return_values
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+   array.each do |item|
+     if item.length > 4
+       return true
+     end
+   end
+  return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+# famous_cats.index("Puss in Boots")
+  array.each do |item|
+    if cheese_types.index(item) != nil
+      return cheese_types[cheese_types.index(item)]
+    end
+  end
+  return nil
 end

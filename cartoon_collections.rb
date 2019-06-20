dda2b02084bf54604718c0ae3fe8cb279d8a97c9
Michael McCullough <michael.mccullough21@gmail.com>
2019-06-20 16:07:51 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require "pry"
+def roll_call_dwarves(array)
+  order = array.sort_by{|word| word.length}
+count = 1
+  order.each do |dwarf|
+    puts "#{count}. #{dwarf}"
+    count += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(array)
+   output = []
+   array.each do |thing|
+     output << "#{thing.capitalize}!"
+   end
+  output
 end
 
 def long_planeteer_calls# code an argument here

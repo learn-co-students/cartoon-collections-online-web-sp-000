6a91eace28e17b3dffe08eaa7902fae2d2601eab
Peter Grant <jgrantps@hotmail.com>
2019-06-21 02:24:11 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |i, index|
+    number =  index + 1
+    puts "#{number} #{i}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+family = ["louise", "rosie", "lillian", "pickles"]
+def summon_captain_planet(sentence)
+  array_capitals = sentence.each {|a| a.capitalize!}
+  array_split = sentence.collect { |e| e.split("")}
+  array_mark = array_split.each {|i| i.push "!" }
+  array_group = array_split.collect {|u| u.join("")}
+  return array_group
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(input)
+  input.any? { |i| i.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(list)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  list.detect { |x| cheese_types.include?(x) }
 end

476e5391078d14e920fa4b5f1ae560b4583878c7
Bernadette Davis <bsd231@nyu.edu>
2019-07-04 20:22:29 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(arrayOfDwarves)# code an argument here
   # Your code here
+  arrayOfDwarves.each_with_index {|element, index|
+    puts "#{index + 1}. #{element}"
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arrayOfCalls)# code an argument here
   # Your code here
+  newArray = []
+  arrayOfCalls.each {|call|
+    string = call[0].upcase + call[1..-1] + "!"
+    newArray << string
+  }
+  return newArray
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(arrayOfCalls)# code an argument here
   # Your code here
+  arrayOfCalls.any? {|call|
+  call.length > 4
+}
+
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.each {|element|
+    return element if cheese_types.any?{ |type|
+      type == element
+    }
+  }
+  return nil
 end

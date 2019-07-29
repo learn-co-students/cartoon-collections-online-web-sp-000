f002d87d752f789d8daa850b689f8be7fbd8ccc8
Christopher Dent <christopherdent01@gmail.com>
2019-07-29 01:48:24 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,65 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Dopey", "Doc", "Sleepy", "Grumpy"]
+def roll_call_dwarves(dwarves)
+  
+  dwarves.each.with_index(1) do |dwarf, index| 
+    puts "#{index} #{dwarf}"
+  end 
+  
+end
+roll_call_dwarves(dwarves)
+
+
+###########################################################################
+
+summons = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(summons)
+  
+  summons.map! do |summon|
+    summon.capitalize  + "!"
+  end 
+ p summons
+end
+summon_captain_planet(summons)
+
+
+############################################
+
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+#####################################################################################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+def find_the_cheese(array)
+cheeses = ["cheddar", "gouda", "camembert"]
+
+array.find do |item|
+  cheeses.include?(item)
+end 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+
+##.detect RETURNS FIRST TRUE VALUE 
+
+#.include?  returns true if the given object is present in self 
+#(that is, if any element == object), otherwise returns false.
+
+
+
+
+

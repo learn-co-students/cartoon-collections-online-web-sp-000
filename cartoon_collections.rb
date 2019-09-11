38bacae7436974383f2961d9d1b2be2d6c20c9e8
Blythe Pollard <blytheepollard@gmail.com>
2019-09-11 21:59:12 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,15 +7,27 @@ end
 def summon_captain_planet(array)
   new_array = []
   array.collect do |element|
-    new_array << "#{element}"
+    "#{element.capitalize}!"
 end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.collect do |element|
+    if element.length < 4
+      return true
+    else
+      return false
+    end
+  #take array
+  #if any element of array > 4, false
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.collect do |type_of_food|
+    if type_of_food.include?("cheddar" || "gouda" || "camembert")
+      return type_of_food
+  end
+end
 end

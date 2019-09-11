ad126685caa4f4ae41e907663ba3a1a63f4ffa2d
Blythe Pollard <blytheepollard@gmail.com>
2019-09-11 19:37:04 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,14 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+    puts "#{index + 1}. #{name}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  array.collect do |element|
+    new_array << "#{element}"
+end
 end
 
 def long_planeteer_calls# code an argument here

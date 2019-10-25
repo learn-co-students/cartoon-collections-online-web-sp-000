cec4b1fa7dcc79de3831ee315b9492bb8981f68e
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-25 22:03:51 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves =["Doc", "Dopey", "Bashful", "Grumpy"]
+ def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+roll_call_dwarves(dwarves)
+
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+summon_captain_planet(planeteer_calls)
+
+
+planeteer_calls = ["two", "go", "industrious", "bop"]
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+long_planeteer_calls(planeteer_calls)
+
+
+snacks = ["crackers", "gouda", "thyme"]
+def find_the_cheese(snacks)
+  cheeses = ["gouda", "cheddar", "camembert"]
+    snacks.find do |cheese|
+    cheeses.include?(cheese)
+  end
 end
+
+find_the_cheese(snacks)

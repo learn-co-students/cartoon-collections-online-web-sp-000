f3a063a2f9d20f7b5c4fdb0f829680267e72608a
Sweet Van Loan <sweet@sweetvanloan.com>
2019-12-01 20:37:10 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves) 
+  dwarves.each do puts "#{index} #{dwarves}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer) # code an argument here
+  planeteer.map! {|name| name.capitalize}
+  planeteer.collect {|name| name + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls) # code an argument here
   # Your code here
+  calls.each do {|call| call.length > 4?}
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strings) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  strings.include(cheese_types)
 end

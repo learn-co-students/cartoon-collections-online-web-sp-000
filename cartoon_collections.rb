37cf2780ab468ba61d2e1d9f9925d3b739c630a0
Joshua A Jack <mynameisjoshuajack@gmail.com>
2020-02-29 00:16:15 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+ dwarves = ["1.Dopey", "2.Grumpy", "3.Bashful"]
+ puts dwarves
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+def summon_captain_planet(veggies)
+  veggies.collect do |call| 
+    call.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(array)
+  array.each do |call|
+    if call.length < 4
+      return true 
+    else return false 
+    end
+    result
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(types)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  types.find do |cheese| 
+    cheese_types.include?(cheese)
 end
+end
+

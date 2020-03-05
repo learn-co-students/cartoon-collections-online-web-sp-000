4d6fdfa73fcfe353a580f64483ff30e5603face9
C. Leow <chienleow1@gmail.com>
2020-03-05 03:15:27 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,9 +7,9 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(planeteer_calls)
-  ## binding.pry
-  planeteer_calls.map { |item| item.capitalize }
-  puts "#{item}!"
+  binding.pry
+  planeteer_calls.map { |item| item.capitalize
+  puts "#{item}!" }
 end
 
 def long_planeteer_calls# code an argument here

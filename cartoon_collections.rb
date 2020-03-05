122540d58bd3d70cf67d6a3dbd79102b58f19d22
C. Leow <chienleow1@gmail.com>
2020-03-05 03:34:31 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,14 +7,17 @@ def roll_call_dwarves(names)
 end
 
 def summon_captain_planet(planeteer_calls)
+  ##binding.pry
   planeteer_calls.map do |item| 
     item.capitalize
-    puts "#{item}!" 
+    item.split
+    return "#{item}!" 
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
+  
 end
 
 def find_the_cheese# code an argument here

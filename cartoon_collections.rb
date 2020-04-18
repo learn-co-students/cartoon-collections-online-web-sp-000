f48b6e4c597ef6639b844f21a6404af4454fe1e8
Andrew Bourgeois <andrew@olympicmedia.com>
2020-04-18 20:27:13 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,40 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+# –––––––––––––––––––––––––––––
+def roll_call_dwarves(dwarves)
+  i = 0
+  while i < dwarves.length
+    dwarves.each do |dwarf|
+      dwarf_index = (dwarves.index(dwarf) + 1).to_s
+      puts "#{dwarf_index}. #{dwarf}"
+    end
+    i += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+# –––––––––––––––––––––––––––––
+def summon_captain_planet(elements)
+  elements.map do |element|
+    element.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+# –––––––––––––––––––––––––––––
+def long_planeteer_calls(calls)
+  # calls_to_string = calls.join(" ")
+  # check = /\w{4,}/.match(calls_to_string)
+  # check == nil ? false : true
+  #  
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end

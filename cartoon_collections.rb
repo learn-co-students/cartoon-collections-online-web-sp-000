65087d0053ffbf3f8669ef6f488e33ae11c613f2
Jung Lee <lee.jung.s95@gmail.com>
2020-04-25 05:37:08 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarve_list)# code an argument here
+  i = 1
+  dwarve_list.each do |dwarve|
+    puts "#{i}. #{dwarve}"
+    i += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(veggies)
+  veggies.collect do |veg|
+    veg.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(call)# code an argument here
+  if call.any?{|i| i.length > 4}
+    return true
+  else
+    return false
+  end# Your code here
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheddar_cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheddar_cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end

e2deb009e5bff4e4acc167360fa87907eb0a0ef7
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-12 14:27:29 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |item, index|
+    index += 1
+    puts "#{index}. #{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map! do |name|
+    name << "!"
+    name.capitalize()
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |call|
+    call.length > 4
+  end
+    
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect do |name| 
+    name == cheese_types[0] || name == cheese_types[1] || name == cheese_types[2]
+  end
 end

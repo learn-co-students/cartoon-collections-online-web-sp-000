7e3e678769c3a637e33533afc5c2035f3d962f8a
reevesalic <reevesalic@gmail.com>
2020-06-17 14:00:38 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 short_words = ["puff", "go", "two"]
+snacks = ["crackers", "gouda", "thyme"]
+cheese_types = ["cheddar", "gouda", "camembert"]
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
 i = 0
@@ -25,13 +27,15 @@ end
 summon_captain_planet(planeteer_calls)
 
 
-def long_planeteer_calls(short_words)# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
-  assorted_words = ["two", "go", "industrious", "bop"]
-  long_planeteer_calls(assorted words)
+  array.any?{|word|word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
 end
+

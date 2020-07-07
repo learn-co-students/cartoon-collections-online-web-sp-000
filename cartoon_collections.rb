ebd669c0bef77fef4ddc63ecf0d68b878f8e1614
Leslie G <lesliegonzalez@me.com>
2020-07-07 22:48:56 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,45 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+names = %w[Doc Dopey Bashful Grumpy Sneezy Sleepy Happy]
+
+def roll_call_dwarves(names)
+  names.each_with_index do |dwarf, index|
+  puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+roll_call_dwarves(names)
+
+
+
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map do |elements|
+  "#{elements.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+summon_captain_planet(planeteer_calls)
+
+
+
+short_words = ["puff", "go", "two"]
+assorted_words = ["two", "go", "industrious", "bop"]
+
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+long_planeteer_calls(planeteer_calls)
+
+
+
+food_items = []
+
+def find_the_cheese(food_items)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food_items.find do |mystery_food|
+  cheese_types.include?(mystery_food)
+  end
 end
+  
+find_the_cheese(food_items)  

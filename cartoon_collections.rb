ea1323ec9355b67648a234e0c6fe7b4a94debf18
mirthful-concept-6030 <jchristinem@gmail.com>
2020-07-20 01:09:30 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(summons)
+  summons.collect do |summon|
+    "#{summon.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.size > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food_items)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food_items.find do |food|
+    cheese_types.include?(food)
+  end
 end

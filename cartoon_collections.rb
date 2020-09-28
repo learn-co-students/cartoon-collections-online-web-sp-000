f75adeaf549ff53282fa86392324b769bc600885
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-09-28 01:09:29 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect { |i| i.capitalize << "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  if 
+   array.any? { |i| i.length > 4 }
+    return true 
+ else
+   array.all? { |i| i.length > 4 }
+    return false 
+ end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find { |i| i == "cheddar" || i == "gouda" || i == "camembert" }
 end

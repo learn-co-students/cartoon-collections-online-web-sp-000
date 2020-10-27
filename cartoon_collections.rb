e1b58198da8867098711e26a1f05a1dc19c69b64
Jacob W Jones <jacobwadejones76@gmail.com>
2020-10-27 19:35:41 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,18 +1,19 @@
 def roll_call_dwarves(array)
-  array.each_with_index {|name, inedex| puts "#{index + 1}. #{name}"}
+  array.each_with_index{|name, index| puts "#{index + 1}. #{name}"}
   
 end
 
 def summon_captain_planet(array)
+  array.map{|word| word.capitalize.insert(-1, "!")}
   
 end
 
 def long_planeteer_calls(array)
+  array.any?{|word| word.length > 4}
   
 end
 
 def find_the_cheese(array)
-  
-  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find{|cheese| cheese_types.include?(cheese)}
 end

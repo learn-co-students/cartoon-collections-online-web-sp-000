0f5e939d7082bf99c13bbddb7b8efc88f3bdebc7
Chase-Alan Baldwin <alan.chasebaldwin@gmail.com>
2020-11-09 04:47:00 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  names.each_with_index {|i, a| puts "#{a + 1}. #{i}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map { |e| e.capitalize() + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  if calls.any? { |i| i.length > 4 } == true
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  cheese_types.each do |i|
+    if cheese.include?(i) == true
+       return i
+    else
+        return nil
+    end
+  end
 end

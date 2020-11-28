2aaa8c94539709b072418440e6b6664f13912717
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-28 23:45:16 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,39 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)
+  dwarves = ["Dopey", "Grumpy", "Bashful", "Sleepy", "Doc", "Sneezy", "Happy"]
+  dwarves.each_with_index do |names, index|
+    puts "#{index+1}. #{names}"
+end
+end 
+
+def summon_captain_planet(array)
+#planateer_calls = ["earth", "wind", "fire", "water", "heart"]
+#veggies = ["carrot", "cucumber", "pepper"]
+#fruits = ["applie", "banana", "orange"]
+value = []
+array.collect do |phrase|
+  value << phrase.upcase + "!"
+end
+puts value
+return value 
+end 
+ 
+   
+    # it 'returns an array with the same number of elements that it was given' do
+    #   veggies = %w[carrot cucumber pepper]
+    #   result = summon_captain_planet(veggies)
+    #   expect(result.class).to eq(Array) 
+    #   expect(result.length).to eq(3)
+    # end
+
+
+
+def long_planeteer_calls
+# code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
+def find_the_cheese
+# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  # cheese_types = ["cheddar", "gouda", "camembert"]
+end 

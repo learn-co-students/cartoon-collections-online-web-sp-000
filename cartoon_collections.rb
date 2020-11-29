d37472ddf92240ac947addb51b83c80f7cbe1b70
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-29 23:41:14 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,6 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 end 
 
+
 def summon_captain_planet(array)
   value = []
   array.collect do |phrase|
@@ -14,17 +15,18 @@ def summon_captain_planet(array)
 end 
 
 
-
-
-
 def long_planeteer_calls(words)
   [words].any? { |word| word.length <= 4 }
      end 
 
 
- 
-def find_the_cheese 
-# code an argument here
-  # the array below is here to help
-  # cheese_types = ["cheddar", "gouda", "camembert"] 
-end  
+
+def find_the_cheese(ingredients)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  str = cheese_types.join(" ")
+  ingredients.each do |element|
+  if element.include?(str) 
+    puts element
+  end 
+end 
+end 

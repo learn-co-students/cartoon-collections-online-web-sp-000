4745cf39dd2b31c68808faae4197700b8bc98e7e
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-29 01:48:02 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,18 +18,13 @@ end
 
 
 def long_planeteer_calls(words)
-  words.each do |call|
-  if call.length.any? > 4
-      true 
-      false 
-  end
-end 
-end 
+  [words].any? { |word| word.length <= 4 }
+     end 
 
 
  
-def find_the_cheese
+def find_the_cheese 
 # code an argument here
   # the array below is here to help
-  # cheese_types = ["cheddar", "gouda", "camembert"]
-end 
+  # cheese_types = ["cheddar", "gouda", "camembert"] 
+end  

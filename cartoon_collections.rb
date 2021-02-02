13faff9a0ce7df346f5a70f881489c33fcfcb53d
Devorah Posner <dleahposner@gmail.com>
2021-02-02 00:41:24 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,11 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  
+  # found_cheese = nil
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |food| 
+     cheese_types.include?(food)
+      
+  end
+
 end

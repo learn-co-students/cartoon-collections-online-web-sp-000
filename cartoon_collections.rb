dca570c288a34ebcb30ede8c404d688cd7b7c660
langan.kg Langan <langan.kg@gmail.com>
2021-03-26 16:23:10 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
+  def roll_call_dwarves(dwarves)
+    dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+    dwarves.each_with_index {|name, index| puts "#{index +1}. #{name}"}
+  end
+
+  def summon_captain_planet(planeteer_calls)
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+    summon_captain_planet do |element|
+      puts " #{element.capitalise}" +"!"
+        planeteer_calls << yield (array[element])
+    end
+    planeteer_calls
+  end
 
 def long_planeteer_calls# code an argument here
   # Your code here
@@ -14,3 +21,13 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+#def my_find(collection)
+#  i = 0
+#  while i < collection.length
+  #  if yield(collection[i])
+#      return collection[i]
+#    end
+  #  i = i + 1
+#  end
+ #end

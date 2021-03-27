9744601f308e4548c0ee9018fc11d42000da7b6d
langan.kg Langan <langan.kg@gmail.com>
2021-03-27 11:06:42 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,33 +1,25 @@
 
+  dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
   def roll_call_dwarves(dwarves)
-    dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
     dwarves.each_with_index {|name, index| puts "#{index +1}. #{name}"}
   end
 
-  def summon_captain_planet(planeteer_calls)
   planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-    summon_captain_planet do |element|
-      puts " #{element.capitalise}" +"!"
-        planeteer_calls << yield (array[element])
-    end
-    planeteer_calls
+  def summon_captain_planet(planeteer_calls)
+      planeteer_calls.map { |word| word.capitalize + '!'}
   end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+  assorted_words = ["two", "go", "industrious", "bop"]
+  def long_planeteer_calls(assorted_words)
+    assorted_words.any? do |word|
+      word.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
-
-#def my_find(collection)
-#  i = 0
-#  while i < collection.length
-  #  if yield(collection[i])
-#      return collection[i]
-#    end
-  #  i = i + 1
-#  end
- #end

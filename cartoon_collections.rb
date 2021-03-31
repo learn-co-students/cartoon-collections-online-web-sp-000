9895cf6198aabdccfd954c2ceafccdc65585e8b1
Erika Curry-Elrod <ecurryelrod@gmail.com>
2021-03-31 17:54:22 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
+require 'pry'
+
+def roll_call_dwarves(names) # code an argument here
   # Your code here
+  names.each.with_index(1) {|name, index| puts "#{index}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls) # code an argument here
+  # Your code here 
+  planeteer_calls.collect {|calls| "#{calls.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls) # code an argument here
   # Your code here
+  calls.any? do |call| 
+    if call.length > 4
+      true 
+    end
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese| 
+    if cheese_types.include?(cheese)
+      true
+    end
+  end
 end

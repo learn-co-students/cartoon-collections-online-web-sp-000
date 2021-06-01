82f27c7b27eb3bf77d427a908831f2c420f4b781
Ian King <kickthechair@gmail.com>
2021-06-01 14:47:51 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,41 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |value, index| 
+    puts "#{index + 1} #{value}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+def summon_captain_planet(array)
+  array.map do |element|
+    element.capitalize.concat("!")
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls(array)
+  array.any? { |element| element.size > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+
+
+def find_the_cheese(array)
+  if array.include?("cheddar")
+    "cheddar"
+  elsif array.include?("goudar")
+    "gouda"
+  elsif array.include?("camembert")
+    "camembert"
+  else
+    nil
+  end
+end
+
+
+
+
+#The #find_the_cheese method using .include? can be refactored by calling the #find method.
+def find_the_cheese(array)
+  array.find { |element| element == "cheddar" || element == "goudar" || element == "camembert" }
 end
